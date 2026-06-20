11d350c75c765f86b770bd621c9f97a6128856c2
Maksym <112901851+Maksym82@users.noreply.github.com>
2026-06-20 21:29:00 +0300

    Update banner

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-	
-	
-
-}, false);
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    const swiperBanner = new Swiper('.swiper-banner', {
+      speed: 700,
+      spaceBetween: 10,
+      navigation: {
+        prevEl: '.swiper-banner .swiper-button-prev',
+        nextEl: '.swiper-banner .swiper-button-next',
+      },
+      pagination: {
+        el: '.swiper-banner .swiper-pagination',
+        clickable: true,
+      },
+    });
+  },
+  false,
+);
