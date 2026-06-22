770635040fe58ffad297d36fe1af8a9ed637a926
Maksym <112901851+Maksym82@users.noreply.github.com>
2026-06-22 15:41:52 +0300

    Update gallery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener(
   'DOMContentLoaded',
   function () {
+    // Banner START
     const swiperBanner = new Swiper('.swiper-banner', {
       speed: 700,
       spaceBetween: 10,
@@ -13,6 +14,37 @@ document.addEventListener(
         clickable: true,
       },
     });
+    // Banner END
+
+    // Gallery START
+
+    // init Masonry
+    var $grid = $('.grid').masonry({
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      gutter: 20,
+    });
+    $grid.imagesLoaded().progress(function () {
+      $grid.masonry('layout');
+    });
+    $('.magnific-image').magnificPopup({
+      type: 'image',
+      mainClass: 'mfp-with-zoom',
+      zoom: {
+        enabled: true,
+
+        duration: 500,
+        easing: 'ease-in-out',
+
+        opener: function (openerElement) {
+          return openerElement.is('img')
+            ? openerElement
+            : openerElement.find('img');
+        },
+      },
+    });
+    // Gallery END
   },
   false,
 );
